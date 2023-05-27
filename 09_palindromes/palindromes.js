34d464f95fd7962f64371bb72f1440d9f6c1a43c
Luqman <bluebearymuffin55@gmail.com>
2023-05-27 22:20:30 +0800

    Finished the first two of the test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    let newWords = words.toLowerCase().split("!, ").reverse().join("! ");
+    //let newWords2 = newWords[-1].toUpperCase().join("! ");
+    if(newWords === words) return true;
+    //if(newWords2 === words) return true; Not finished yet
 };
 
 // Do not edit below this line
